4888c1d22916ae4d015257145bbfc6011e8626a2
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-10-31 16:02:56 +0300

    слот1

--- src/main/java/com/uchiha/uchiha/mod/GatherDataHandler.java
+++ src/main/java/com/uchiha/uchiha/mod/GatherDataHandler.java
@@ -3,9 +3,7 @@ package com.uchiha.uchiha.mod;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = "uchiha", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GatherDataHandler {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
