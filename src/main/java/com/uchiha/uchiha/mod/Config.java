063cc050f112066d0e460dffe762b7b44bd493f8
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-10-31 16:36:20 +0300

    слот4

--- src/main/java/com/uchiha/uchiha/mod/Config.java
+++ src/main/java/com/uchiha/uchiha/mod/Config.java
@@ -1,17 +1,24 @@
 package com.uchiha.uchiha.mod;
 
+import java.util.List;
+
 public class Config {
-    public static final Object SPEC = null;
+    public static final IConfigSpec SPEC = new DummyConfigSpec();
     public static final DummyConfigValue LOG_DIRT_BLOCK = new DummyConfigValue();
     public static final DummyConfigValue MAGIC_NUMBER_INTRODUCTION = new DummyConfigValue();
     public static final DummyConfigValue MAGIC_NUMBER = new DummyConfigValue();
     public static final DummyConfigValue ITEM_STRINGS = new DummyConfigValue();
 
-    // Эмулируем методы get, getAsBoolean, getAsInt чтобы не было ошибки компиляции
+    public static class DummyConfigSpec implements IConfigSpec {}
+
+    public interface IConfigSpec {} // Это просто "заглушка" интерфейса
+
     public static class DummyConfigValue {
         public boolean getAsBoolean() { return false; }
         public String get() { return ""; }
         public int getAsInt() { return 0; }
-        public void forEach(java.util.function.Consumer<String> action) {}
+        public void forEach(java.util.function.Consumer<String> action) {
+            for (String s : List.of("ItemA", "ItemB")) action.accept(s);
+        }
     }
 }
