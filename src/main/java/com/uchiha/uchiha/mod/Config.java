7157e6d716bdd8ec7404acb0d7159074306ee618
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-10-31 16:34:10 +0300

    слот4

--- src/main/java/com/uchiha/uchiha/mod/Config.java
+++ src/main/java/com/uchiha/uchiha/mod/Config.java
@@ -1,5 +1,17 @@
 package com.uchiha.uchiha.mod;
 
 public class Config {
-    // Этот класс пустой, просто чтобы не было ошибки компиляции!
+    public static final Object SPEC = null;
+    public static final DummyConfigValue LOG_DIRT_BLOCK = new DummyConfigValue();
+    public static final DummyConfigValue MAGIC_NUMBER_INTRODUCTION = new DummyConfigValue();
+    public static final DummyConfigValue MAGIC_NUMBER = new DummyConfigValue();
+    public static final DummyConfigValue ITEM_STRINGS = new DummyConfigValue();
+
+    // Эмулируем методы get, getAsBoolean, getAsInt чтобы не было ошибки компиляции
+    public static class DummyConfigValue {
+        public boolean getAsBoolean() { return false; }
+        public String get() { return ""; }
+        public int getAsInt() { return 0; }
+        public void forEach(java.util.function.Consumer<String> action) {}
+    }
 }
