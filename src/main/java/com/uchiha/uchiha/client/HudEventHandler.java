dfa40031797d368c4748c5a8e5f34be20851e777
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-10-31 22:58:28 +0300

    Твой комментарий к коммиту (например: Исправлен главный класс)

--- src/main/java/com/uchiha/uchiha/client/HudEventHandler.java
+++ src/main/java/com/uchiha/uchiha/client/HudEventHandler.java
@@ -1,17 +1,24 @@
 package com.uchiha.uchiha.client;
 
 import com.uchiha.uchiha.magic.MagicData;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.event.RenderGuiOverlayEvent;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = "uchiha", value = Dist.CLIENT)
 public class HudEventHandler {
+
     @SubscribeEvent
-    public static void onRenderHUD(RenderGuiOverlayEvent.Post event) {
+    public static void onRenderHUD(RenderGameOverlayEvent.Post event) {
+        if (event.getType() != RenderGameOverlayEvent.ElementType.ALL) return;
+
+        GuiGraphics guiGraphics = new GuiGraphics(event.getMatrixStack(), event.getWindow().getWidth(), event.getWindow().getHeight());
+        // Создаем тестовый объект маны
         MagicData testData = new MagicData(100);
         testData.setMana(50);
-        ManaHud.render(event.getGuiGraphics(), testData, 10, 10);
+
+        ManaHud.render(guiGraphics, testData, 10, 10);
     }
 }
