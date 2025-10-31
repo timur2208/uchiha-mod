7f9e035315a0b49051ec45a4227ecf144a0a3811
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-10-31 15:41:12 +0300

    слот+++

---
+++ src/main/java/com/uchiha/uchiha/mod/GatherDataHandler.java
@@ -0,0 +1,19 @@
+package com.uchiha.uchiha.mod;
+
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.neoforged.bus.api.SubscribeEvent;
+
+public class GatherDataHandler {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        event.getGenerator().addProvider(
+                event.includeServer(),
+                new UchihaCuriosDataProvider(
+                        event.getGenerator().getPackOutput(),
+                        event.getExistingFileHelper(),
+                        event.getLookupProvider()
+                )
+        );
+    }
+}
