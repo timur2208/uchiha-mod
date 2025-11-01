37f85cb1a5018f2fe95f56f191857ae3d0341b29
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-11-01 16:47:00 +0300

    Твой комментарий, например: добавил отрисовку HUD

--- src/main/java/com/uchiha/uchiha/magic/ManaTickHandler.java
+++ src/main/java/com/uchiha/uchiha/magic/ManaTickHandler.java
@@ -1,24 +1,22 @@
 package com.uchiha.uchiha.magic;
 
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
-@Mod.EventBusSubscriber(modid = "uchiha")
 public class ManaTickHandler {
 
-    @SubscribeEvent
     public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
         PlayerManaData.initializePlayer(event.getEntity());
     }
 
-    @SubscribeEvent
     public static void onServerTick(ServerTickEvent.Pre event) {
-        if (event.getServer() != null) {
-            event.getServer().getPlayerList().getPlayers().forEach(player -> {
+        MinecraftServer server = event.getServer();
+        if (server != null) {
+            for (Player player : server.getPlayerList().getPlayers()) {
                 PlayerManaData.regenerateMana(player);
-            });
+            }
         }
     }
 }
--- src/main/java/com/uchiha/uchiha/mod/uchiha.java
+++ src/main/java/com/uchiha/uchiha/mod/uchiha.java
@@ -5,8 +5,12 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.slf4j.Logger;
 import com.uchiha.uchiha.client.HudEventHandler;
+import com.uchiha.uchiha.magic.ManaTickHandler;
 
 @Mod("uchiha")
 public class uchiha {
@@ -17,6 +21,14 @@ public class uchiha {
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::clientSetup);
         modEventBus.addListener(HudEventHandler::registerGuiLayers);
+
+        NeoForge.EVENT_BUS.addListener((PlayerEvent.PlayerLoggedInEvent event) -> {
+            ManaTickHandler.onPlayerLoggedIn(event);
+        });
+
+        NeoForge.EVENT_BUS.addListener((ServerTickEvent.Pre event) -> {
+            ManaTickHandler.onServerTick(event);
+        });
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
