d96abcadb211dfbf89f3fc7bfd5bcfcc352ae145
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-11-01 17:12:42 +0300

    Твой комментарий, например: добавил отрисовку HUD

--- src/main/java/com/uchiha/uchiha/client/ClientTickHandler.java
+++ src/main/java/com/uchiha/uchiha/client/ClientTickHandler.java
@@ -2,12 +2,9 @@ package com.uchiha.uchiha.client;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
-import net.neoforged.api.distmarker.Dist;
 import com.uchiha.uchiha.magic.PlayerManaData;
 
-@Mod.EventBusSubscriber(modid = "uchiha", value = Dist.CLIENT)
 public class ClientTickHandler {
 
     @SubscribeEvent
--- src/main/java/com/uchiha/uchiha/mod/uchiha.java
+++ src/main/java/com/uchiha/uchiha/mod/uchiha.java
@@ -6,10 +6,12 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import org.slf4j.Logger;
 import com.uchiha.uchiha.client.HudEventHandler;
+import com.uchiha.uchiha.client.ClientTickHandler;
 import com.uchiha.uchiha.magic.ManaTickHandler;
 
 @Mod("uchiha")
@@ -22,12 +24,18 @@ public class uchiha {
         modEventBus.addListener(this::clientSetup);
         modEventBus.addListener(HudEventHandler::registerGuiLayers);
 
+        // Серверные события
         NeoForge.EVENT_BUS.addListener((PlayerEvent.PlayerLoggedInEvent event) -> {
             ManaTickHandler.onPlayerLoggedIn(event);
         });
 
         NeoForge.EVENT_BUS.addListener((ServerTickEvent.Pre event) -> {
             ManaTickHandler.onServerTick(event);
+        });
+
+        // Клиентские события
+        NeoForge.EVENT_BUS.addListener((ClientTickEvent.Post event) -> {
+            ClientTickHandler.onClientTick(event);
         });
     }
 
