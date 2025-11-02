c269371cb9ef9a1a1c5bab8c8cbff72398a9afed
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-11-02 15:52:30 +0300

    перестройка

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven { url = uri("https://maven.neoforged.net/releases") }
+    }
+}
+
 rootProject.name = "uchiha-mod"
