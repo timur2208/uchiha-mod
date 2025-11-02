53e3fc330677d202e1e62c32e4dffa4a546e2ed2
Timur Karchigaev <timurkarcigaev605@gmail.com>
2025-11-02 15:50:03 +0300

    перестройка

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("net.neoforged.gradle.userdev") version "7.0.105"
+}
+
+group = "com.uchiha"
+version = "1.0.0"
+base.archivesName.set("uchiha-mod")
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.neoforged.net/releases/")
+    }
+}
+
+dependencies {
+    implementation("net.neoforged:neoforge:21.1.211")
+}
+
+tasks.named("jar", Jar::class) {
+    from("LICENSE") {
+        rename { "${it}_${base.archivesName.get()}" }
+    }
+}
